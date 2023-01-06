b43a4adfd52a2621da970a4956ce838d0ccf6acb
KelwinGerson <kelwingerson@outlook.com>
2023-01-06 08:26:34 -0300

    feat: home left column

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,12 +17,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { ProductsHeaderComponent } from './components/pages/home/components/products-header/products-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsHeaderComponent
   ],
   imports: [
     BrowserModule,
