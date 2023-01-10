d20dd285b0ccc64fb31e73a882b3d6fbfb624b5f
KelwinGerson <kelwingerson@outlook.com>
2023-01-10 08:02:48 -0300

    feat: categories filter

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   cols = 3
+  category: string | undefined;
   constructor() { }
 
   ngOnInit(): void {
@@ -15,4 +16,9 @@ export class HomeComponent implements OnInit {
   onColumsCountChange(colsNum: number): void {
     this.cols = colsNum;
   }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+  }
 }
+
